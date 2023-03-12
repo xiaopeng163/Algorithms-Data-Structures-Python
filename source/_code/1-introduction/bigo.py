a0328e6967299ab6c60224b486d3234aed452d3f
Peng Xiao <xiaoquwl@gmail.com>
2023-03-13 00:29:18 +0100

    update

--- source/_code/1-introduction/bigo.py
+++ source/_code/1-introduction/bigo.py
@@ -1,6 +1,19 @@
-def O_of_n(n):
+# O(n)
+
+
+def bigo(n):
     for i in range(n):
         print(i)
+        for j in range(n):
+            print(j)
 
 
-O_of_n(10)
+bigo(10)
+
+# O(n^2)
+
+# O(1)
+
+a = [1, 2, 3]
+
+a[0]
