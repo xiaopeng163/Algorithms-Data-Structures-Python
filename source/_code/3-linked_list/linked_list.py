f4ab856908743ee860142223c9be0bbc53340a37
Peng Xiao <xiaoquwl@gmail.com>
2023-03-20 23:33:28 +0100

    update

--- source/_code/3-linked_list/linked_list.py
+++ source/_code/3-linked_list/linked_list.py
@@ -8,51 +8,42 @@ class LinkedList:
     def __init__(self) -> None:
         self.head = None
         self.tail = None
-        self.length = 1
 
     def print_list(self):
-        temp = self.head
-        while temp:
-            print(temp.data, end=" ")
-            temp = temp.next
-            if temp:
-                print("->", end=" ")
+        tmp = self.head
+        while tmp:
+            print(tmp.data, end=" ")
+            tmp = tmp.next
+            if tmp:
+                print("-> ", end=" ")
         print()
 
     def append(self, value):
         new_node = Node(value)
-        if self.head is None:
+        if self.head == None:
             self.head = new_node
-            self.tail = self.head
+            self.tail = new_node
         else:
             self.tail.next = new_node
             self.tail = new_node
-        self.length += 1
 
-    def prepend(self, value) -> None:
+    def prepend(self, value):
         new_node = Node(value)
-        new_node.next = self.head
-        self.head = new_node
-        self.length += 1
-
-    def reverse(self):
-        prev = None
-        current = self.head
-        while current is not None:
-            next = current.next
-            current.next = prev
-            prev = current
-            current = next
-        self.head = prev
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
 
 
 if __name__ == "__main__":
 
-    linked_list = LinkedList()
-    linked_list.append(1)
-    linked_list.append(3)
-    linked_list.append(5)
-    linked_list.append(7)
-    linked_list.append(9)
-    linked_list.prepend(0)
-    linked_list.print_list()
+    linkedlist = LinkedList()
+    linkedlist.append(1)
+    linkedlist.append(2)
+    linkedlist.append(3)
+
+    linkedlist.print_list()
+    linkedlist.prepend(0)
+    linkedlist.print_list()
