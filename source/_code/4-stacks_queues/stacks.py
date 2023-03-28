a6529162f74ee29457f4779498729b1c0f4244ae
Peng Xiao <xiaoquwl@gmail.com>
2023-03-28 21:50:49 +0200

    update

--- source/_code/4-stacks_queues/stacks.py
+++ source/_code/4-stacks_queues/stacks.py
@@ -1,49 +1,20 @@
-# stack implementation using a linked list.
-
-
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-
 class Stack:
     def __init__(self):
-        self.top = None
-        self.size = 0
+        self.items = []
 
-    def push(self, data):
-        node = Node(data)
-        node.next = self.top
-        self.top = node
-        self.size += 1
+    def push(self, item):
+        self.items.append(item)
 
     def pop(self):
-        if self.top:
-            data = self.top.data
-            self.top = self.top.next
-            self.size -= 1
-            return data
-        else:
-            return None
+        if not self.is_empty():
+            return self.items.pop()
 
     def peek(self):
-        if self.top:
-            return self.top.data
-        else:
-            return None
+        if not self.is_empty():
+            return self.items[-1]
 
     def is_empty(self):
-        return self.size == 0
+        return len(self.items) == 0
 
-    def get_size(self):
-        return self.size
-
-    def print_stack(self):
-        if self.top:
-            node = self.top
-            while node:
-                print(node.data)
-                node = node.next
-        else:
-            print("Stack is empty.")
+    def size(self):
+        return len(self.items)
