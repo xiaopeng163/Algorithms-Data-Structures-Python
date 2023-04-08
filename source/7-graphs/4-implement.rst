c4a14254b7391a24bdf7f4916ddebcc4e34f3a8a
Peng Xiao <xiaoquwl@gmail.com>
2023-04-08 07:36:37 +0200

    update

---
+++ source/7-graphs/4-implement.rst
@@ -0,0 +1,9 @@
+用Python实现一个Graph
+==========================
+
+
+基于邻接列表的存储方式，用Python实现一个Graph，要求实现初始化，添加删除节点和边。
+
+.. literalinclude:: ../_code/7-graph/graph.py
+   :language: python
+   :linenos:
---
+++ source/_code/7-graph/graph.py
@@ -0,0 +1,9 @@
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list:
+            self.adj_list[vertex] = []
+            return True
+        return False
