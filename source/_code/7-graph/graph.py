c93f79d9114723acbdda1af4005207c530c7310f
Peng Xiao <xiaoquwl@gmail.com>
2023-04-09 07:37:55 +0200

    update

--- source/_code/7-graph/graph.py
+++ source/_code/7-graph/graph.py
@@ -1,9 +1,43 @@
 class Graph:
     def __init__(self):
+
         self.adj_list = {}
 
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ":", self.adj_list[vertex])
+
     def add_vertex(self, vertex):
+
         if vertex not in self.adj_list:
             self.adj_list[vertex] = []
             return True
         return False
+
+    def add_edge(self, v1, v2):
+
+        if v1 in self.adj_list and v2 in self.adj_list:
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+
+    def delete_edge(self, v1, v2):
+        if v1 in self.adj_list and v2 in self.adj_list:
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+
+
+graph = Graph()
+graph.add_vertex("A")
+graph.add_vertex("B")
+graph.add_edge("A", "B")
+graph.add_edge("A", "B")
+graph.print_graph()
+graph.delete_edge("A", "B")
+graph.print_graph()
