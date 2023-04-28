971598d16da7d4015dfbbb49ccdfd23c5b1cfda3
Peng Xiao <xiaoquwl@gmail.com>
2023-04-28 06:43:17 +0200

    update

--- source/_code/11-merge-sort/merge-sort.py
+++ source/_code/11-merge-sort/merge-sort.py
@@ -1,6 +1,6 @@
 def merge(l1, l2):
-    # merge to sorted list
     result = []
+
     i = j = 0
 
     while i < len(l1) and j < len(l2):
@@ -8,9 +8,15 @@ def merge(l1, l2):
         if l1[i] > l2[j]:
             result.append(l2[j])
             j += 1
+
         elif l1[i] < l2[j]:
             result.append(l1[i])
             i += 1
+        else:
+            result.append(l1[i])
+            result.append(l2[j])
+            i += 1
+            j += 1
 
     while i < len(l1):
         result.append(l1[i])
@@ -19,18 +25,22 @@ def merge(l1, l2):
     while j < len(l2):
         result.append(l2[j])
         j += 1
+
     return result
 
 
 def merge_sort(my_list):
+
     if len(my_list) == 1:
         return my_list
+
     mid_index = int(len(my_list) / 2)
+
     left = merge_sort(my_list[:mid_index])
     right = merge_sort(my_list[mid_index:])
 
     return merge(left, right)
 
 
-result = merge_sort([6, 5, 3, 1, 8, 7, 2, 4])
+result = merge_sort([6, 6, 3, 1, 8, 7, 2, 6])
 print(result)
